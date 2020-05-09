a160647c88186682f14f457062f666b84486fcf8
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-09 12:36:05 -0300

    Criado o crud para members

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,8 +1,10 @@
-const moment = require("moment")
-
+const Member = require("../models/member")
+const moment = require("moment");
 module.exports = {
   index(req, res) {
-    return res.render("../views/members/index")
+    Member.all(function (members) {
+      return res.render("members/index", { members })
+    })
   },
   create(req, res) {
     return res.render("members/create")
@@ -10,28 +12,46 @@ module.exports = {
   saveOrUpdate(req, res) {
 
     const keys = Object.keys(req.body)
-
+    const { id } = req.body
     for (key of keys) {
       if (req.body[key] == "") {
         return res.send("Por favor, preencha todos os campos!")
       }
     }
 
-    let { id, name, avatar_url, birth, gender, created_at, email, blood, weight, height } = req.body;
-
-    return;
+    if (!id) {
+      Member.create(req.body, function (id) {
+        return res.redirect(`/members/${id}`)
+      })
+    } else {
+      Member.update(req.body, function (id) {
+        return res.redirect(`/members/${id}`)
+      })
+    }
   },
   show(req, res) {
     const { id } = req.params
-    return
+
+    Member.find(id, function (member) {
+      if (!member) return res.send("Instrutor não encontrado! :(")
+      member.age = moment().diff(member.birth, 'years', false)
+      member.gender === "M" ? "Masculino" : "Feminino";
+      member.created_at = moment(member.created_at).format('L')
+      return res.render("members/show", { member })
+    })
   },
   edit(req, res) {
     const { id } = req.params
-    return
-    //return foundMember != null ? res.render("../views/members/create", { member }) : res.send("Não encontrou");
+    Member.find(id, function (member) {
+      member.birth = moment(member.birth).add(1, "d").format('YYYY-MM-DD')
+      return member != null ? res.render("members/create", { member }) : res.send("Não encontrou");
+    })
   },
   delete(req, res) {
     const { id } = req.body;
+    Member.find(id, function (member) {
+      member != null ? Member.delete(id) : res.send("Membro não existe")
+    })
     return res.redirect(`/members`)
   },
 }
--- src/app/models/member.js
+++ src/app/models/member.js
@@ -2,20 +2,20 @@ const db = require("../../config/db")
 const moment = require("moment");
 module.exports = {
   all(callback) {
-    db.query("SELECT * FROM instructors", function (err, results) {
-      if (err) throw "Database Error!" + err
+    db.query("SELECT * FROM members", function (err, results) {
+      if (err) throw "Database Error!"
       callback(results.rows)
     })
   },
   create(data, callback) {
     const query = `
-    INSERT INTO instructors (
-      avatar_url, name, services, gender, birth, created_at
-    ) VALUES ($1,$2,$3,$4,$5,$6)
+    INSERT INTO members (
+      name, avatar_url, birth, gender, created_at, email, blood, weight, height
+    ) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)
     RETURNING id
   `;
     const values = [
-      data.avatar_url, data.name, data.services, data.gender, data.birth, moment().calendar()
+      data.name, data.avatar_url, data.birth, data.gender, moment().calendar(), data.email, data.blood, data.weight, data.height
     ]
 
 
@@ -26,7 +26,7 @@ module.exports = {
   },
   find(id, callback) {
     const query = `
-    SELECT * FROM instructors where id = ${id}
+    SELECT * FROM members where id = ${id}
   `;
     db.query(query, function (err, results) {
       if (err) throw "Database Error!" + err
@@ -36,14 +36,14 @@ module.exports = {
   },
   update(data, callback) {
     const query = `
-      UPDATE instructors SET
-        avatar_url = ($1), name = ($2), services = ($3), gender = ($4), birth = ($5) 
-      WHERE ID = $6
+      UPDATE members SET
+        name = ($1), avatar_url = ($2), birth = ($3), gender = ($4), email = ($5), blood =($6), weight = ($7), height = ($8)
+      WHERE ID = $9
       RETURNING id
       
   `;
     const values = [
-      data.avatar_url, data.name, data.services, data.gender, data.birth, data.id
+      data.name, data.avatar_url, data.birth, data.gender, data.email, data.blood, data.weight, data.height, data.id
     ]
 
     db.query(query, values, function (err, results) {
@@ -53,11 +53,10 @@ module.exports = {
   },
   delete(id) {
     const query = `
-    DELETE FROM instructors where id = ${id}
+    DELETE FROM members where id = ${id}
   `;
     db.query(query, function (err, results) {
       if (err) throw "Database Error!" + err
-      console.log(results);
     })
   },
 
