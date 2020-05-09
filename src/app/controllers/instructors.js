9c936950b32fe7c5893df84f71f2e38ec1f534e9
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-09 01:09:51 -0300

    Inserido o metodo de salvar

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const moment = require("moment")
+const db = require("../../config/db")
 
 module.exports = {
   index(req, res) {
@@ -16,10 +17,23 @@ module.exports = {
         return res.send("Por favor, preencha todos os campos!")
       }
     }
+    console.log(moment().calendar());
 
-    let { id, avatar_url, birth, name, services, gender } = req.body;
 
-    return;
+    let { id, avatar_url, name, services, gender, birth } = req.body;
+    const query = `
+      INSERT INTO instructors (
+        avatar_url, name, services, gender, birth, created_at
+      ) VALUES ($1,$2,$3,$4,$5,$6)
+      RETURNING id
+    `;
+    const values = [
+      avatar_url, name, services, gender, birth, moment().calendar()
+    ]
+    db.query(query, values, function (err, results) {
+      if (err) return res.send("Database Error!");
+      return res.redirect(`/instructors/${results.rows[0].id}`)
+    })
   },
   show(req, res) {
     const { id } = req.params
