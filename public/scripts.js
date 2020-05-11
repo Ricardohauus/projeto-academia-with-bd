2b693c0edf5ae6648c5e4edda19cfb014544fda6
Ricardo Brasil <ricardohauus@gmail.com>
2020-05-11 09:24:47 -0300

    Lógica para paginação

--- public/scripts.js
+++ public/scripts.js
@@ -1,16 +1,37 @@
-const currentPage = location.pathname;
+const currentPageIndex = location.pathname;
 const menuItems = document.querySelectorAll("header .links a")
 
 for (menuItem of menuItems) {
-  console.log(currentPage, menuItem.getAttribute("href"));
-  if (currentPage.includes(menuItem.getAttribute("href"))) {
+  if (currentPageIndex.includes(menuItem.getAttribute("href"))) {
     menuItem.classList.add("active")
     break;
   }
-
-
 }
 
+
+let totalPages = 4,
+  selectedPage = 19,
+  pages = [],
+  oldPage
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+  if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push("...")
+    }
+
+    if (oldPage && currentPage - oldPage == 2) {
+      pages.push(currentPage - 1)
+    }
+    pages.push(currentPage)
+    oldPage = currentPage
+  }
+}
+console.log(pages);
 
 
 
